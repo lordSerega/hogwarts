6e15c24ae243de710bb3f3b7660d10d503c1191b
Kaplin Sergey <kaplin@internet.ru>
2022-04-11 14:37:21 +0300

    Выполнил дз 2. Создание API. Swagger UI, Postman

---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,57 @@
+package ru.hogwarts.school.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RestController
+@RequestMapping("student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Student> readStudent(@PathVariable Long id) {
+        Student student = studentService.readStudent(id);
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @PutMapping
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student) {
+        Student foundStudent = studentService.updateStudent(student);
+        if (foundStudent == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(foundStudent);
+    }
+
+    @DeleteMapping("{id}")
+    public Student deleteStudent(@PathVariable Long id) {
+        return studentService.deleteStudent(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Student>> findStudentsByAge(@RequestParam int age) {
+        if (age > 0) {
+            return ResponseEntity.ok(studentService.findStudentsByAge(age));
+        }
+        return ResponseEntity.ok(Collections.emptyList());
+    }
+}
